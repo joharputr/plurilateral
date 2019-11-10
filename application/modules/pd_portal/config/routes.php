b7f2b1db83fa6621bab13ad04f7ffb4601907673
Kurniaji Gunawan <dawn_shadow96@yahoo.co.id>
2019-11-10 10:06:54 +0700

    Controller event dkk

--- application/modules/pd_portal/config/routes.php
+++ application/modules/pd_portal/config/routes.php
@@ -15,13 +15,18 @@ $route['portal/document/save'] = 'pd_portal/document/insert_document';
 $route['portal/document/edit'] = 'pd_portal/document/update_dokumen';
 $route['portal/document/hapus/(:num)'] = 'pd_portal/document/hapus_document/$1';
 $route['portal/document/edit/(:num)'] = 'pd_portal/document/edit_document/$1';
-$route['portal/login'] = 'pd_portal/login';
+
 $route['portal/document/national'] = 'pd_portal/document/national_initiative';
 $route['portal/document/tambah_national'] = 'pd_portal/document/tambah_national';
 $route['portal/document/edit_national'] = 'pd_portal/document/edit_national';
 $route['portal/document/workshop'] = 'pd_portal/document/workshop';
 $route['portal/document/tambah_workshop'] = 'pd_portal/document/tambah_workshop';
 $route['portal/document/edit_workshop'] = 'pd_portal/document/edit_workshop';
+
+//Bagian ini yang kau tambahin
+$route['portal/event']= 'pd_portal/Kegiatan/index';
+
+$route['portal/login'] = 'pd_portal/login';
 $route['portal/message'] = 'pd_portal/document/message';
 // $route['event/about'] = 'pd_event/event/index/about';
 // $route['event/goal'] = 'pd_event/event/index/goal';
--- application/modules/pd_portal/controllers/Kegiatan.php
+++ application/modules/pd_portal/controllers/Kegiatan.php
@@ -16,90 +16,11 @@ class Kegiatan extends MY_Controller
 
     public function index()
     {
-        $data['title'] = "Plurilateral Document";
-        $data['private_url'] = "document";
-        $data['show'] = $this->db->query("SELECT * FROM dokumen")->result();
-        $this->load->view('content/dokumen/document', $data);
+        $data['title'] = "Plurilateral Event";
+        $data['private_url'] = "event";
+        $data['show'] = $this->db->query("SELECT * FROM kegiatan")->result();
+        $this->load->view('content/event/kegiatan', $data);
     }
 
-    public function tambah_document()
-    {
-        $data['title'] = "Tambah Plurilateral Document";
-        $data['private_url'] = "tambah_document";
-        $data['kegiatan'] = $this->db->query("SELECT * FROM kegiatan")->result();
-        $this->load->view('content/dokumen/tambah_document', $data);
-    }
-
-    function insert_document()
-    {
-        $judul = $this->input->post('judul');
-        /*  $dokumen= $this->input->post('dokumen');*/
-        $event = $this->input->post('event');
-        $tipe = $this->input->post('tipe');
-
-        $object = array(
-            'judul' => $judul,
-            'dokumen' => 'assets/files/' . $this->_uploadImage3(),
-            'event' => $event,
-            'tipe' => $tipe,
-        );
-        //     $this->dokumen = $this->_uploadImage3();
-        $test = $this->db->insert('dokumen', $object);
-
-
-        redirect('portal/document', 'refresh');
-    }
-
-
-    private function _uploadImage3()
-    {
-        $config['upload_path']          = './assets/files/';
-        $config['allowed_types']        = 'gif|jpg|png|pdf';
-        $config['file_name']            = time();
-        $config['overwrite']            = true;
-        $config['max_size']             = 25600; // 1MB
-        // $config['max_width']            = 1024;
-        // $config['max_height']           = 768;
-        $this->load->library('upload', $config);
-
-        if ($this->upload->do_upload('dokumen')) {
-            return $this->upload->data('file_name');
-        } else {
-            return 'default.jpg';
-        }
-    }
-
-    function hapus_document($id)
-    {
-        $delete = $this->db->delete('dokumen', array('id' => $id));
-        redirect('portal/document', 'refresh');
-    }
-
-    function edit_document($id)
-    {
-        $data['title'] = "Edit Plurilateral Document";
-        $data['editdata']  = $this->db->get_where('dokumen', array('id' => $id))->result_object();
-        $data['kegiatan'] = $this->db->query("SELECT * FROM kegiatan")->result();
-        $this->load->view('content/dokumen/edit_document', $data);
-    }
-
-    function update_dokumen()
-    {
-
-        $id = $this->input->post('id');
-        $judul = $this->input->post('judul');
-        $event = $this->input->post('event');
-        $tipe = $this->input->post('tipe');
-
-        $object = array(
-            'judul' => $judul,
-            'dokumen' => 'assets/files/' . $this->_uploadImage3(),
-            'event' => $event,
-            'tipe' => $tipe,
-        );
-        //     $this->dokumen = $this->_uploadImage3();
-        $test = $this->db->update('dokumen', $object, array('id' => $id));
-
-        redirect('portal/document', 'refresh');
-    }
+    //buat fungsi nampilin bisa liat di controller lain
 }
